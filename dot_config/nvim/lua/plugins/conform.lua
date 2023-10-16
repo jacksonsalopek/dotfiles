28ccac982b7951b50163ae745f9ed6e2e41c3253
Jackson Salopek <jackson.salopek@gmail.com>
2023-10-15 23:56:49 -0400

    Initial commit

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      -- LazyVim will use these options when formatting with the conform.nvim formatter
+      format = {
+        timeout_ms = 1000,
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
+        rust = { "rustfmt" },
+        javascript = { "biome" },
+        typescript = { "biome" },
+        zig = { "zigfmt" },
+        java = { "google-java-format" },
+      },
+      -- LazyVim will merge the options you set here with builtin formatters.
+      -- You can also define any custom formatters here.
+      ---@type table<string,table>
+      formatters = {
+        injected = { options = { ignore_errors = true } },
+        -- # Example of using dprint only when a dprint.json file is present
+        -- dprint = {
+        --   condition = function(ctx)
+        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+        --   end,
+        -- },
+        --
+        -- # Example of using shfmt with extra args
+        -- shfmt = {
+        --   extra_args = { "-i", "2", "-ci" },
+        -- },
+      },
+    },
+  },
+}
