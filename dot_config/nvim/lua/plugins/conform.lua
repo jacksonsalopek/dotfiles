52be8cdeb5e180f45e0b9e7886efe2ccbd69ad83
Jackson Salopek <jackson.salopek@gmail.com>
2023-12-12 15:48:11 -0500

    20231212

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -15,6 +15,7 @@ return {
         typescript = { "biome" },
         zig = { "zigfmt" },
         java = { "google-java-format" },
+        kotlin = { "ktlint" },
       },
       -- LazyVim will merge the options you set here with builtin formatters.
       -- You can also define any custom formatters here.
